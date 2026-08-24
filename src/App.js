dceed63e18324d9086a3b8efee10c4315f3b9788
Soffia-k <sofiyakov2002@yandex.ru>
2026-08-24 16:24:41 +0300

    added fixes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ import { Input } from './components/Input/Input';
 import { List } from './components/List/List';
 import { useState } from "react";
 
+// const task = {
+//   id: 1,
+//   taskName: 'feed a cat',
+//   status: 'new'
+// }
+
 function App() {
   const [value, setValue] = useState('');
   const [values, setValues] = useState([]);
-  const handleSubmit = (value) => {
-    if (value !== '' && value !== ' ') {
-      setValues([...values, value])
-    }
+  const handleSubmit = (value, e) => {
+    e.preventDefault();
+    setValues(prev => [...prev, { id: Date.now(), taskName: value, status: 'new' }]);
   }
   return (
     <div className='App'>
       <Header />
-      <Input onChangeValue={setValue} onSubmit={() => handleSubmit(value)} value={value} />
-      <List list={values} />
+      <form onSubmit={(e) => {handleSubmit(value, e)}}>
+        <input onChange={(e) => setValue(e.target.value)} value={value} />
+        <button type="submit">Add</button>
+        <button type="button" onClick={() => setValue('')}>Clear</button>
+      </form>
+      <ol>
+        {values.map((item) => <li key={item.id}>{item.taskName}</li>)}
+      </ol>
     </div>
   );
 }

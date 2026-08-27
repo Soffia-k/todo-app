8ca11fd0c4a1cf774337d64690177626331bc3ee
Soffia-k <sofiyakov2002@yandex.ru>
2026-08-27 17:08:11 +0300

    added delete feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import { Header } from './components/Header/Header';
-import { Input } from './components/Input/Input';
-import { List } from './components/List/List';
+// import { Input } from './components/Input/Input';
+// import { List } from './components/List/List';
 import { useState } from "react";
+
+import { Button } from 'antd';
 
 // const task = {
 //   id: 1,
@@ -15,18 +17,30 @@ function App() {
   const [values, setValues] = useState([]);
   const handleSubmit = (value, e) => {
     e.preventDefault();
-    setValues(prev => [...prev, { id: Date.now(), taskName: value, status: 'new' }]);
+    if (value !== '' && value !== ' ') {
+      setValues(prev => [...prev, { id: Date.now(), taskName: value, status: 'new' }]);
+    }
+    setValue('');
   }
   return (
     <div className='App'>
       <Header />
-      <form onSubmit={(e) => {handleSubmit(value, e)}}>
+      <form onSubmit={(e) => { handleSubmit(value, e) }}>
         <input onChange={(e) => setValue(e.target.value)} value={value} />
-        <button type="submit">Add</button>
-        <button type="button" onClick={() => setValue('')}>Clear</button>
+        <Button type="primary" htmlType="submit">Submit</Button>
+        <Button onClick={() => setValue('')}>Clear</Button>
       </form>
       <ol>
-        {values.map((item) => <li key={item.id}>{item.taskName}</li>)}
+        {values.map((item) =>
+          <li className="listItem" key={item.id}>{item.taskName}
+            <button onClick={() => {
+              values.forEach((item) => console.log(item));
+              setValues(
+                values.filter((value) => value.id !== item.id)
+              );
+            }}>Delete</button>
+          </li>
+        )}
       </ol>
     </div>
   );

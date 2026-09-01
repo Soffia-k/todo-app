43f3016affd2c87493adc52f66b90f91a40ec98e
Soffia-k <sofiyakov2002@yandex.ru>
2026-09-01 17:09:34 +0300

    altered list logic

--- src/App.js
+++ src/App.js
@@ -1,47 +1,40 @@
 import './App.css';
+import { CustomList } from './components/List/List';
+
 import { Header } from './components/Header/Header';
-// import { Input } from './components/Input/Input';
-// import { List } from './components/List/List';
 import { useState } from "react";
-
-import { Button } from 'antd';
-
-// const task = {
-//   id: 1,
-//   taskName: 'feed a cat',
-//   status: 'new'
-// }
+import { Button, Input } from 'antd';
 
 function App() {
   const [value, setValue] = useState('');
   const [values, setValues] = useState([]);
+
+
   const handleSubmit = (value, e) => {
     e.preventDefault();
     if (value !== '' && value !== ' ') {
-      setValues(prev => [...prev, { id: Date.now(), taskName: value, status: 'new' }]);
+      setValues(prev => [...prev, { id: Date.now(), taskName: value, status: 'new', dateFinished: '' }]);
     }
     setValue('');
   }
+
   return (
     <div className='App'>
+
       <Header />
+
       <form onSubmit={(e) => { handleSubmit(value, e) }}>
-        <input onChange={(e) => setValue(e.target.value)} value={value} />
-        <Button type="primary" htmlType="submit">Submit</Button>
-        <Button onClick={() => setValue('')}>Clear</Button>
+
+        <Input className='width30' placeholder="What are you up to?" onChange={(e) => setValue(e.target.value)} value={value} />
+
+        <Button style={{ marginLeft: '10px' }} type="primary" htmlType="submit">Submit</Button>
+        <Button style={{ marginLeft: '10px' }} type="text" onClick={() => setValue('')}>Clear</Button>
+
       </form>
-      <ol>
-        {values.map((item) =>
-          <li className="listItem" key={item.id}>{item.taskName}
-            <button onClick={() => {
-              values.forEach((item) => console.log(item));
-              setValues(
-                values.filter((value) => value.id !== item.id)
-              );
-            }}>Delete</button>
-          </li>
-        )}
-      </ol>
+
+      <CustomList values={values.filter((value) => value.status === 'new')} setValues={setValues} />
+      <CustomList values={values.filter((value) => value.status === 'done')} setValues={setValues} />
+
     </div>
   );
 }
